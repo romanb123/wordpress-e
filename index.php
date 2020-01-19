7cda4cbd9ae74f2d624be6c826cfcc5d722c6175
romanb123 <romanb123@users.noreply.github.com>
2020-01-19 03:45:42 +0200

    hj

--- index.php
+++ index.php
@@ -3,23 +3,57 @@
 <h2>this is the blog page</h2>
 <br>
 <div class="row">
-    <?php
+
+    <!-- =============posts section====================== -->
+    <div class="col-sm-9"> <?php
 while (have_posts()) {
     the_post();?>
 
-
-
-    <div class="card col-sm-3" style="width:300px">
-        <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%">
-        <div class="card-body">
-            <h4 class="card-title"><?php the_title()?></h4>
-            <p class="card-text"><?php the_excerpt()?></p>
-            <a href="<?php the_permalink()?>" class="btn btn-primary">read more</a>
+        <div class="card resent_posts_home col-sm-3">
+            <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%">
+            <div class="card-body">
+                <h4 class="card-title"><?php the_title()?></h4>
+                <p class="card-text"><?php the_excerpt()?></p>
+                <a href="<?php the_permalink()?>" class="btn btn-primary">read more</a>
+            </div>
         </div>
-    </div>
-    <?php }
+        <?php }
 
 ?>
+    </div>
+    <!-- =============posts section====================== -->
+    <div class="col-sm-3 sidebar_right">
+        <!-- ====================================================================== -->
+        <!-- sidebar -->
+        <!-- ====================================================================== -->
+        <?php
+$pages = get_pages();
+foreach ($pages as $page) {?>
+        <?php
+if (get_page_link($page->ID) == get_permalink()) {?>
+        <!-- ==========html=============  -->
+        <li class="list-group-item active sidemenu">
+            <a href="<?php echo get_page_link($page->ID) ?>">
+                <?php echo $page->post_title; ?></a>
+        </li>
+
+        <!-- ==========html=============        -->
+        <?php
+} else {?>
+        <!-- ==========html=============  -->
+        <li class="list-group-item sidemenu">
+            <a href="<?php echo get_page_link($page->ID) ?>">
+                <?php echo $page->post_title; ?></a>
+        </li>
+        <?php }
+    ?>
+        <!-- ==========html=============        -->
+        <?php }
+?>
+        <!-- ====================================================================== -->
+        <!-- sidebar -->
+        <!-- ====================================================================== -->
+    </div>
 
 </div>
 <?php echo paginate_links();
--- page.php
+++ page.php
@@ -7,7 +7,7 @@ while (have_posts()) {
 <!-- ============================================================================================================ -->
 <div class="container-fluid">
     <div class="row">
-        <div class="col-sm-8">
+        <div class="col-sm-9">
             <?php
 $theParent = wp_get_post_parent_id(get_the_ID());
     if ($theParent) {?>
@@ -21,34 +21,37 @@ $theParent = wp_get_post_parent_id(get_the_ID());
             <h1 class="page-banner__title"><?php the_title();?></h1>
             <p><?php the_content();?></p>
         </div>
-        <div class="col-sm-4">
-            <ul class="list-group">
-                <?php
+        <div class="col-sm-3 sidebar_right">
+            <!-- ====================================================================== -->
+            <!-- sidebar -->
+            <!-- ====================================================================== -->
+            <?php
 $pages = get_pages();
     foreach ($pages as $page) {?>
-                <?php
+            <?php
 if (get_page_link($page->ID) == get_permalink()) {?>
-                <!-- ==========html=============  -->
-                <li class="list-group-item active sidemenu">
-                    <a href="<?php echo get_page_link($page->ID) ?>">
-                        <?php echo $page->post_title; ?></a>
-                </li>
+            <!-- ==========html=============  -->
+            <li class="list-group-item active sidemenu">
+                <a href="<?php echo get_page_link($page->ID) ?>">
+                    <?php echo $page->post_title; ?></a>
+            </li>
 
-                <!-- ==========html=============        -->
-                <?php
+            <!-- ==========html=============        -->
+            <?php
 } else {?>
-                <!-- ==========html=============  -->
-                <li class="list-group-item sidemenu">
-                    <a href="<?php echo get_page_link($page->ID) ?>">
-                        <?php echo $page->post_title; ?></a>
-                </li>
-                <?php }
+            <!-- ==========html=============  -->
+            <li class="list-group-item sidemenu">
+                <a href="<?php echo get_page_link($page->ID) ?>">
+                    <?php echo $page->post_title; ?></a>
+            </li>
+            <?php }
         ?>
-                <!-- ==========html=============        -->
-                <?php }
+            <!-- ==========html=============        -->
+            <?php }
     ?>
-
-            </ul>
+            <!-- ====================================================================== -->
+            <!-- sidebar -->
+            <!-- ====================================================================== -->
         </div>
     </div>
 </div>
