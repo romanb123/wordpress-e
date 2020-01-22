53464c4e28f76b9a7f4018865308a84f1b6c5155
romanb123 <romanb123@users.noreply.github.com>
2020-01-23 01:44:55 +0200

    tytyr

--- footer.php
+++ footer.php
@@ -1,5 +1,51 @@
 <div class="jumbotron text-center footer" style="margin-bottom:0">
-    <p>Footer</p>
+    <div class="row">
+        <div class="col-sm-6">
+            <h3>contact us</h3>
+
+            <?php echo do_shortcode('[contact-form-7 id="1234" title="Contact form 1"]'); ?>
+
+        </div>
+
+        <!-- menu section -->
+        <div class="col-sm-3">
+            <h3>pages</h3>
+            <?php
+$pages = get_pages();
+foreach ($pages as $page) {?>
+
+
+            <!-- ==========html=============  -->
+            <li class="list-group-item footermenu">
+                <a href="<?php echo get_page_link($page->ID) ?>">
+                    <?php echo $page->post_title; ?></a>
+            </li>
+
+            <!-- ==========html=============        -->
+            <?php }
+
+?>
+        </div>
+        <!-- posts section -->
+        <div class="col-sm-3">
+            <h3>posts</h3>
+
+            <?php
+$posts = get_posts();
+foreach ($posts as $post) {?>
+
+
+            <!-- ==========html=============  -->
+            <li class="list-group-item footermenu">
+                <a href="<?php echo get_page_link($post->ID) ?>">
+                    <?php echo $post->post_title; ?></a>
+            </li>
+
+            <!-- ==========html=============        -->
+            <?php }?>
+        </div>
+
+    </div>
 </div>
 
 <?php wp_footer();?>
