3d2503c46f5965f3c5c9a315cf3f09362be11626
romanb123 <44100462+romanb123@users.noreply.github.com>
2020-03-19 22:13:17 +0200

    dfs

--- header.php
+++ header.php
@@ -8,6 +8,24 @@
 </head>
 
 <body>
+    <!-- navbar start -->
+    <div class="navigation">
+        <input type="checkbox" class="navigation__checkbox" id="n-toggle">
+        <label for="n-toggle" class="navigation__button">
+            <span class="navigation__icon">&nbsp;</span>
+        </label>
+        <div class="navigation__background">&nbsp;</div>
+        <nav class="navigation__nav">
+            <ul class="navigation__list">
+                <li class="navigation_item"> <a href="dfdf" class="navigation_link">dfgdfg</a></li>
+                <li class="navigation_item"> <a href="dfdf" class="navigation_link">dfgdfg</a></li>
+                <li class="navigation_item"> <a href="dfdf" class="navigation_link">dfgdfg</a></li>
+                <li class="navigation_item"> <a href="dfdf" class="navigation_link">dfgdfg</a></li>
+                <li class="navigation_item"> <a href="dfdf" class="navigation_link">dfgdfg</a></li>
+            </ul>
+        </nav>
+    </div>
+    <!-- navbar end -->
     <nav class="navbar navbar-expand-sm bg-light navbar">
         <?php
 $args = array(
