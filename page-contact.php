c8c39b908550776c22e021f9486b3694691a1536
romanb123 <44100462+romanb123@users.noreply.github.com>
2020-04-13 06:40:57 +0300

    dfg

---
+++ page-contact.php
@@ -0,0 +1,13 @@
+<?php get_header();
+?>
+<div class="contact-row">
+    <div class="contact-section">
+<?php echo do_shortcode( '[contact-form-7 id="146" title="contact"]' ); ?>
+    </div>
+</div>
+
+
+
+<?php
+get_footer();
+?>
